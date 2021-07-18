88a34b5b7bc8eebe3cec80cd745ae1fc682136c3
windows <nvpz1598@gmail.com>
2021-07-18 09:59:30 +0900

    if문 처리하는거에서 막힘

--- app/src/main/java/com/example/login_logic_following_lucid/MainActivity.kt
+++ app/src/main/java/com/example/login_logic_following_lucid/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.login_logic_following_lucid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        contentID.setOnClickListener {
+            Log.d("메인화면로그", "아이디 입력 시도")
+
+
+        }
+
+
+
+        contentPW.setOnClickListener {
+            Log.d("메인화면로그","비밀번호 입력 시도")
+
+
+        }
+
+
+
+
+        clickBtn.setOnClickListener {
+            Log.d("메인화면로그","로그인 버튼 눌림")
+
+            val inputID = contentID.toString()
+
+            val inputPW = contentPW.toString()
+
+            if (inputID  == "admin@test.com" && inputPW == "qwer") {
+
+                Log.d("관리자 로그인","로그인 성공")
+
+                Toast.makeText(this, "관리자 로그인", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
+
+
+
+
     }
 }
