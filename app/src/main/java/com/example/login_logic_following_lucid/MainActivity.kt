d08d4fd1ea4378bf35a54442dee002a5ddfa0339
windows <nvpz1598@gmail.com>
2021-07-18 10:27:57 +0900

    질문: toast 안뜸

--- app/src/main/java/com/example/login_logic_following_lucid/MainActivity.kt
+++ app/src/main/java/com/example/login_logic_following_lucid/MainActivity.kt
@@ -14,39 +14,37 @@ class MainActivity : AppCompatActivity() {
         contentID.setOnClickListener {
             Log.d("메인화면로그", "아이디 입력 시도")
 
+            val inputID = contentID.toString()
+
 
         }
-
 
 
         contentPW.setOnClickListener {
             Log.d("메인화면로그","비밀번호 입력 시도")
 
+            val inputPW = contentPW.toString()
+
 
         }
 
 
-
-
         clickBtn.setOnClickListener {
-            Log.d("메인화면로그","로그인 버튼 눌림")
+            Log.d("메인화면로그","로그인 시도")
 
             val inputID = contentID.toString()
 
             val inputPW = contentPW.toString()
 
-            if (inputID  == "admin@test.com" && inputPW == "qwer") {
-
-                Log.d("관리자 로그인","로그인 성공")
+             if (inputID  == "admin@test.com" && inputPW == "qwer") {
 
                 Toast.makeText(this, "관리자 로그인", Toast.LENGTH_SHORT).show()
+
+                Log.d("관리자 로그인","로그인 성공")
             }
 
 
         }
-
-
-
 
 
     }
